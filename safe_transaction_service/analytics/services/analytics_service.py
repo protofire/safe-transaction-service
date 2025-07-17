a42b8cb8ea386a5bc8a21d7593ac8d4ada2dcd81
Den <dzianis.smalonski@protofire.io>
2025-07-17 13:50:13 +0200

    feat: add Safe statistics endpoint and related analytics tasks

--- safe_transaction_service/analytics/services/analytics_service.py
+++ safe_transaction_service/analytics/services/analytics_service.py
@@ -1,6 +1,6 @@
 import json
 from functools import cache
-from typing import List
+from typing import Dict, List
 
 from safe_transaction_service.utils.redis import get_redis
 
@@ -12,6 +12,7 @@ def get_analytics_service() -> "AnalyticsService":
 
 class AnalyticsService:
     REDIS_TRANSACTIONS_PER_SAFE_APP = "analytics_transactions_per_safe_app"
+    REDIS_SAFE_STATISTICS = "analytics_safe_statistics"
 
     def get_safe_transactions_per_safe_app(self) -> List:
         redis = get_redis()
@@ -20,3 +21,18 @@ class AnalyticsService:
             return json.loads(analytic_result)
         else:
             return []
+
+    def get_safe_statistics(self) -> Dict:
+        redis = get_redis()
+        analytic_result = redis.get(self.REDIS_SAFE_STATISTICS)
+        if analytic_result:
+            return json.loads(analytic_result)
+        else:
+            return {
+                "total_safes": 0,
+                "total_owners": 0,
+                "unique_owners": 0,
+                "balance_wei": 0,
+                "safes_with_balance": 0,
+                "timestamp": None
+            }
